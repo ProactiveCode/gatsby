c85d9efcd492742dbfcbc98225abec893ccb1497
Ryan Hale <ryan@proactivecode.com>
2022-05-06 13:31:22 +0100

    remove con

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,9 +10,6 @@ module.exports = {
     }
   }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
   }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
