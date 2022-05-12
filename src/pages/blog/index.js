38626f985af8e68e64530a9d593d429b02217d39
Ryan Hale <ryan@proactivecode.com>
2022-05-12 16:04:13 +0100

    remove blog for now

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -23,33 +23,7 @@ console.log(data);
 			<Header pageTitle="Home Page">
 				<p>I'm making this by following the Gatsby Tutorial.</p>
 			</Header>
-			<Section identifier="test" bgcolour="grey" smaller="sectionSmall">
-				<div className={c('blogHome')}>
-					<Title line="linePink" containerSize="container800" align="left">
-						<h3 className={"large"}>Passion in<br></br>what we do</h3>
-					</Title>
-					<div className={c('container', 'container800')}>
-						<p>The team like to stay up to date on current trends in design, development and anything digital. There are some topics the team believe are worth sharing with you, so they have put together some articles to help inspire passion and creativity.</p>
-						<StaticImage width={800} alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera" src="../images/carbon.png" />
-					</div>
-				</div>
-				<div className={c('container', 'container1000', 'blogArticles')}>
-					{
-						data.allFile.nodes.map((node) => (
-							<article key={node.childMdx.id}>
-								<Link to={`/blog/${node.childMdx.slug}`}><h3>{node.childMdx.frontmatter.title}</h3></Link>
-								<p>{node.childMdx.frontmatter.excerpt}</p>
-								<div className="articleInfo">
-									<p><strong>{node.childMdx.frontmatter.author}</strong></p>
-									<p><em>{node.childMdx.frontmatter.author_role}</em></p>
-									<p><strong>{node.childMdx.frontmatter.date}</strong></p>
-									<p><em>{node.childMdx.frontmatter.keywords}</em></p>
-								</div>
-							</article>
-						))
-					}
-				</div>
-			</Section>
+		
 			<Footer></Footer>
       	</main>
     )
